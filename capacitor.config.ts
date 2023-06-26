ff7a6b9bdf9074bf8a63f38dacab768de36ec5bc
dhaine_br <dhaine_br@protonmail.com>
2023-06-26 05:41:18 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MediSearch',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
